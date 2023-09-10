372431a880d69eff9c37ed5343212b7565d7c3c2
BURHAN <Brhn.Sngl.95@outlook.com>
2023-09-10 22:15:08 +0300

    ✨Project setup and basic structure✨

--- src/Utility/MyFunction.java
+++ src/Utility/MyFunction.java
@@ -4,10 +4,12 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class MyFunction {
     public static void Bekle(int sn){
@@ -24,5 +26,18 @@ public class MyFunction {
         String tarih = tarihFormat.format(date);
         File screenShot = ss.getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(screenShot, new File("src/screenshots/screenshot_" + tarih + ".png"));
+    }
+    public static int RandomGenerator(int Limit){
+        return (int)(Math.random()*Limit);
+    }
+    public static boolean ListContainsString(List<WebElement> List, String Aranan){
+        boolean Bulundu = false;
+        for (WebElement X : List){
+            if (X.getText().toLowerCase().equals(Aranan.toLowerCase())){
+                Bulundu = true;
+                break;
+            }
+        }
+        return Bulundu;
     }
 }
